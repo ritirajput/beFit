c97fbccf059f87164a4788b5388ff8aca38c81e5
ritirajput <ritiraj3022@gmail.com>
2024-11-16 21:01:55 -0500

    slpash screen changes in xml done

--- app/src/main/java/com/example/befit/login.java
+++ app/src/main/java/com/example/befit/login.java
@@ -1,9 +1,6 @@
 package com.example.befit;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.window.SplashScreen;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static int delay = 3000;
+public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        new Handler().postDelayed(()->{
-            Intent intent = new Intent(SplashScreen)
-        })
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
---
+++ app/src/main/java/com/example/befit/splashScreen.java
@@ -0,0 +1,28 @@
+package com.example.befit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashScreen extends AppCompatActivity {
+
+    private static int delay = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(()->{
+            Intent intent = new Intent(splashScreen.this,login.class);
+            startActivity(intent);
+            finish();
+        },delay);
+    }
+}
